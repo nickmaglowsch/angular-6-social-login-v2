42f9cf7ac68ecfcacd3712594e4be739427a8b85
Anton Pavlov <askid.mobile@gmail.com>
2018-06-14 23:17:13 +0300

    add Vkontakte SocialNetwork

---
+++ src/providers/vkontakte-login-provider.ts
@@ -0,0 +1,73 @@
+import { BaseLoginProvider } from '../entities/base-login-provider';
+import { SocialUser, LoginProviderClass } from '../entities/user';
+
+declare let VK: any;
+
+export class VkontakteLoginProvider extends BaseLoginProvider {
+
+    public static readonly PROVIDER_ID = 'vkontakte';
+    public loginProviderObj: LoginProviderClass = new LoginProviderClass();
+
+    constructor(private clientId: string) {
+        super();
+        this.loginProviderObj.id = clientId;
+        this.loginProviderObj.name = 'vkontakte';
+        this.loginProviderObj.url = 'https://vk.com/js/api/openapi.js';
+    }
+
+    initialize(): Promise<SocialUser> {
+        return new Promise((resolve, reject) => {
+            this.loadScript(this.loginProviderObj, () => {
+
+                VK.init({
+                    apiId: this.clientId
+                });
+
+                VK.Auth.login( function(response: any){
+                    if (response.status === 'connected') {
+                        VK.Api.call('users.get', {user_id: response.session.mid, fields: 'photo_max,contacts', v: '5.78'},
+                             (res: any) => {
+                                 resolve(VkontakteLoginProvider.drawUser(Object.assign({}, {token: response.sig}, res[0])));
+
+                            }
+                        );
+                    }
+                });
+            });
+        });
+    }
+
+    static drawUser(response: any): SocialUser {
+        let user: SocialUser = new SocialUser();
+        user.id = response.id;
+        user.name = response.first_name + ' ' + response.last_name;
+        user.token = response.token;
+        user.image = response.photo_max;
+        return user;
+    }
+
+    signIn(): Promise<SocialUser> {
+        return new Promise<SocialUser>( (resolve, reject) => {
+            VK.Auth.login(function (response: any) {
+                if (response.status === 'connected') {
+                    VK.Api.call('users.get', {user_id: response.session.mid, fields: 'photo_max,contacts', v: '5.78'},
+                        (res: any) => {
+                            resolve(VkontakteLoginProvider.drawUser(Object.assign({}, {token: response.sig}, res[0])));
+
+                        }
+                    );
+                }
+            });
+        });
+    }
+
+    signOut(): Promise<any> {
+        return new Promise((resolve, reject) => {
+            VK.Auth.logout((response: any) => {
+                resolve();
+            });
+        });
+    }
+
+
+}
