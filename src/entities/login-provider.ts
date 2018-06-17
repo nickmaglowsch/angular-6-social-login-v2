97b0b946409e4e8de514e2ea18a0ee4ab0c9b740
Anton Pavlov <askid.mobile@gmail.com>
2018-06-17 14:42:36 +0300

    First commit

---
+++ src/entities/login-provider.ts
@@ -0,0 +1,9 @@
+import { SocialUser } from './user';
+
+export interface LoginProvider {
+  initialize(): Promise<SocialUser>;
+  getStatus(): Promise<SocialUser>;
+  signIn(): Promise<SocialUser>;
+  signOut(): Promise<any>;
+}
+
---
+++ src/providers/index.ts
@@ -0,0 +1,5 @@
+
+export { GoogleLoginProvider } from './google-login-provider';
+export { FacebookLoginProvider } from './facebook-login-provider';
+export { LinkedinLoginProvider } from './linkedin-login-provider';
+export { VkontakteLoginProvider } from './vkontakte-login-provider';
